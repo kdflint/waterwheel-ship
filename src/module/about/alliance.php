ccd1aad39cb19f0e0fe95e0d3a10055288d3f050
Kathy Flint <kathy.flint@northbridgetech.org>
2015-04-07 16:42:15 -0500

    add partner graphics

--- src/module/about/alliance.php
+++ src/module/about/alliance.php
@@ -13,11 +13,17 @@ require_once("../core/domain/Util.php");
 
 <div class="leaders">
 	<div id="allianceIndex0" class="mod-leader-frame mod-about-normalText" style="visibility:visible;opacity:1;top:120px;">
-	&nbsp;
+		<p class="mod-about-leaderText">These are just a few of the partners that Northbridge serves.</p>
+		<img src="<?php echo Util::getHttpAboutPath(); ?>/images/cat.gif" width="223" height="100" style="padding-top:5px;padding-right:20px;"/>
+		<img src="<?php echo Util::getHttpAboutPath(); ?>/images/fh.png" width="234" height="115" style="padding-top:5px;" />
+		<img src="<?php echo Util::getHttpAboutPath(); ?>/images/cfcht.png" width="365" height="88" style="padding-top:5px;" />
+		<img src="<?php echo Util::getHttpAboutPath(); ?>/images/cfhr.png" width="300" height="50" style="padding-top:0px;" />
+		<img src="<?php echo Util::getHttpAboutPath(); ?>/images/edc.png" width="182" height="116" style="padding-top:5px;" />
+
 	</div>
 
 	<div id="allianceIndex1" class="mod-leader-frame" style="top:120px;"">
-		<p class="mod-about-leaderText"><span style="float:right;padding:5px;"><span class="fa fa-cogs fa-2x"></span> <a class="mod-about-anchor" href="<?php echo Util::getHttpCorePath(); ?>/index.php?view=volunteer">Volunteer</a></span>Frankly, there is no such thing as a Northbidge volunteer who is not a leader. It really only seems right to name each and every one!</p>
+		<p class="mod-about-leaderText"><span style="float:right;padding:5px;"><span class="fa fa-cogs fa-2x"></span> <a class="mod-about-anchor" href="<?php echo Util::getHttpCorePath(); ?>/index.php?view=volunteer">Volunteer</a></span>Without our volunteers we've got nothing. With them we change the world. It really only seems right to name each and every one!</p>
 		<p class="mod-about-leaderText">But since that's not possible in this space, and since our teams do enjoy a little friendly competition amongs themselves, we settle for naming a Team of the Month.</p>
 		<p class="mod-about-header">Volunteer Team of the Month</p>
 		<p class="mod-about-normalText">Team North Stars continues to break ground not only by developing features for the pilot deployment of Nexus but also for refining the Northbridge methods for volunteer engagement. The North Stars are</p>
