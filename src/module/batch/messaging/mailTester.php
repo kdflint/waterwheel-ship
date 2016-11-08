3e5588486d5ddf10fb328a3e8b8a4b810484c435
Kathy Flint <kathy.flint@northbridgetech.org>
2016-11-07 20:06:12 -0600

    update info email with information video

---
+++ src/module/batch/messaging/mailTester.php
@@ -0,0 +1,97 @@
+#!/usr/bin/php
+
+<?php
+    mail("ksflint@comcast.net",
+        "This is the test message subject8",
+        "This is the test message body8 - comcast",
+        "From: Northbridge Technology Alliance <contact@northbridgetech.org>"); // . "\r\n" . "Content-Type: text/plain; charset=utf-8");
+        //"From: Northbridge Technology Alliance <contact@northbridgetech.org>" . "\r\n" . "Content-Type: text/plain; charset=utf-8",
+        //"-fcontact@northbridgetech.org");
+        
+
+mail("ksflint@comcast.net", "Partnership Information", "--placeholder-boundry
+Content-Type: text/plain; charset="iso-8859-1"
+Content-Transfer-Encoding: 7bit
+
+Hello ,
+
+Here is the information that you requested recently. Questions? Just reply to this email and a real person will get right back to you.
+
+Northbridge creates software solutions for charitable organizations who are engaged in social justice and community-building efforts so that their collective impact is increased.
+
+Partnership with Northbridge means that an organization or team has demonstrated to Northbridge that their activities are focused on social justice and community-building and is therefore eligible to benefit from the charitable services that Northbridge provides.
+
+Those benefits include:
+
+* the privilege to participate in moderated conversations about the technical needs of the social justice community
+
+* exclusive access to the Nexus Web Conference Center
+
+* exclusive access to our growing suite of web-based collaborative tools
+
+
+Application for partnership with Northbridge takes just a few minutes from a tablet sized device or larger.
+
+http://northbridgetech.org/index.php?view=apply_form
+
+You may enjoy a brief webinar hosted by Founder and CEO Kathy Flint. In this live, informal setting, Kathy will talk with you about the benefits of partnership with Northbridge Technology Alliance and answer your questions.
+
+https://www.eventbrite.com/e/information-webinar-tickets-16317817030
+
+This is how one Northbridge partner describes his experience with Northbridge and the Nexus Web Conference Center.
+
+https://youtu.be/tk-QNJruZgM
+
+Whitepaper: Partnership with Northbridge Technology Alliance
+
+http://northbridgetech.org/downloads/Northbridge_partnership.pdf
+
+Whitepaper: Nexus Web Conference Center
+
+http://northbridgetech.org/downloads/Northbridge_web_conference_center.pdf
+
+We hope these resources are helpful. You may reply to this email with any questions at all. You will reach a real person who wants to help your social mission succeed. We hope to hear from you!
+
+Our regards,
+
+Northbridge Technology Alliance
+
+=================
+This message is produced and distributed by Northbridge Technology Alliance, a United States 501(c)(3) nonprofit corporation, Evanston, Illinois  60202
+--placeholder-boundry
+Content-Type: text/html; charset='iso-8859-1'
+Content-Transfer-Encoding: 7bit
+
+<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 4.01//EN'>
+<html>
+	<head>
+		<link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Oxygen'>
+	</head>
+	<body style='font-family:Oxygen,Arial,sans-serif;color:#484848;'>
+		<table style='width:90%;display:block;max-width:620px;'>
+			<tr>
+				<td colspan='2'><img src='http://northbri.wwwls12.a2hosted.com/apps/waterwheel/module/core/images/NB_horizontal_rgb.png' alt='Northbridge Technology Alliance Logo' width='252' height='68' style='padding-bottom:10px;padding-right:30px;'/></td>
+			</tr>
+			<tr>
+				<td style='vertical-align:top;padding-top:10px;'>
+					<a href='https://twitter.com/'NorthbridgeNFP' target='_blank'><img src='http://northbri.wwwls12.a2hosted.com/apps/waterwheel/module/core/images/twitter_dae0bc_32.png' width='32' height=32' /></a><br/>
+					<a href='//plus.google.com/u/0/101145194341428988499?prsrc=3' rel='publisher' target='_blank' style='text-decoration:none;'><img src='http://northbri.wwwls12.a2hosted.com/apps/waterwheel/module/core/images/google-plus-square_dae0bc_32.png' width='32' height=32' /></a><br/>
+					<a href='https://www.linkedin.com/company/2232384' target='_blank'><img src='http://northbri.wwwls12.a2hosted.com/apps/waterwheel/module/core/images/linkedin_dae0bc_32.png' width='32' height=32' /></a><br/>
+					<a href='https://www.facebook.com/northbridgenfp#' target='_blank'><img src='http://northbri.wwwls12.a2hosted.com/apps/waterwheel/module/core/images/facebook-square_dae0bc_32.png' width='32' height=32' /></a><br/>
+					<a href='https://github.com/NorthBridge/playbook/wiki/1.How-We-Do' target='_blank'><img src='http://northbri.wwwls12.a2hosted.com/apps/waterwheel/module/core/images/github_dae0bc_32.png' width='32' height=32' /></a>
+				</td>
+				<td style='vertical-align:top;padding-left:10px;'><p>We are so glad you have discovered Northbridge Technology Alliance!</p><p>Here is the information that you requested recently. Questions? Just reply to this email and a real person will get right back to you.</p><p>Northbridge creates software solutions for charitable organizations who are engaged in social justice and community-building efforts so that their collective impact is increased.</p><p>Partnership with Northbridge means that an organization or team has demonstrated to Northbridge that their activities are focused on social justice and community-building and is therefore eligible to benefit from the charitable services that Northbridge provides.</p><p>Those benefits include:</p><p>* the privilege to participate in moderated conversations about the technical needs of the social justice community</p><p>* exclusive access to the Nexus Web Conference Center</p><p>* exclusive access to our growing suite of web-based collaborative tools</p><p>
+Application for partnership with Northbridge takes just a few minutes from a tablet sized device or larger.</p><p><a style='text-decoration:none;font-weight:bold;width:160px;background:none repeat scroll 0% 0% rgba(137, 157, 112, 0.6);margin:10px;border-radius:6px;font-family:Oxygen;font-size:100%;padding:0.5em 1em;color:rgba(0, 0, 0, 0.8);' href='http://northbridgetech.org/index.php?view=apply_form'>Apply Now</a></p><p>You may enjoy a brief webinar hosted by Founder and CEO Kathy Flint. In this live, informal setting, Kathy will talk with you about the benefits of partnership with Northbridge Technology Alliance and answer your questions.</p><p><a style='text-decoration:none;font-weight:bold;width:160px;background:none repeat scroll 0% 0% rgba(137, 157, 112, 0.6);margin:10px;border-radius:6px;font-family:Oxygen;font-size:100%;padding:0.5em 1em;color:rgba(0, 0, 0, 0.8);' href='https://www.eventbrite.com/e/information-webinar-tickets-16317817030'>Register for Webinar</a></p><p>This is how one Northbridge partner describes his experience with Northbridge and the Nexus Web Conference Center.</p><p><a style='text-decoration:none;font-weight:bold;width:160px;background:none repeat scroll 0% 0% rgba(137, 157, 112, 0.6);margin:10px;border-radius:6px;font-family:Oxygen;font-size:100%;padding:0.5em 1em;color:rgba(0, 0, 0, 0.8);' href='https://youtu.be/tk-QNJruZgM'><img src='http://northbridgetech.org/images/video-camera_484848_24.png' width='20' height=20' style='vertical-align:text-bottom;' /> Watch</a></p><p>Whitepaper: Partnership with Northbridge Technology Alliance</p><p><a style='text-decoration:none;font-weight:bold;width:160px;background:none repeat scroll 0% 0% rgba(137, 157, 112, 0.6);margin:10px;border-radius:6px;font-family:Oxygen;font-size:100%;padding:0.5em 1em;color:rgba(0, 0, 0, 0.8);' href='http://northbridgetech.org/downloads/Northbridge_partnership.pdf'><img src='http://northbridgetech.org/images/file-pdf-o_484848_24.png' width='20' height=20' style='vertical-align:text-bottom;' /> Download</a></p><p>Whitepaper: Nexus Web Conference Center</p><p><a style='text-decoration:none;font-weight:bold;width:160px;background:none repeat scroll 0% 0% rgba(137, 157, 112, 0.6);margin:10px;border-radius:6px;font-family:Oxygen;font-size:100%;padding:0.5em 1em;color:rgba(0, 0, 0, 0.8);' href='http://northbridgetech.org/downloads/Northbridge_web_conference_center.pdf'><img src='http://northbridgetech.org/images/file-pdf-o_484848_24.png' width='20' height=20' style='vertical-align:text-bottom;'/> Download</a></p><p>We hope these resources are helpful. You may reply to this email with any questions at all. You will reach a real person who wants to help your social mission succeed. We hope to hear from you!</p><p>Our regards,<br/>Northbridge Technology Alliance</p></td>
+			</tr>
+			<tr><td colspan='2'style='font-size:70%;color:#666666;'><hr color='#dae0bc'/><p>This message is produced and distributed by Northbridge Technology Alliance, a United States 501(c)(3) nonprofit corporation, Evanston, Illinois  60202</td></tr>
+		</table>
+	</body>
+</html>
+--placeholder-boundry--"
+, "From: Northbridge Technology Alliance <contact@northbridgetech.org>
+Bcc: kathy.flint@northbridgetech.org
+MIME-Version: 1.0
+Content-Type: multipart/alternative; boundary=placeholder-boundry");
+
+        
+?>
